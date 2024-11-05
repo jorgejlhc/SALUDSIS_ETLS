73ab76af3734048aa8eadfdef2b6b57c9d152f40
jorgejlhc <jorgejlhc@gmail.com>
2024-11-05 02:19:07 -0500

    Versionamiento ETL REP_DIARIO_RECAUDO

---
+++ REP_DIARIO_RECAUDO/REP_DIARIO_RECAUDO/poms/jobs/process/Recaudo/recconsolidado_4.0/src/main/templates/Job_run_template.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xmx1024M' '-Xmx2048M' '-Xmx4096M' -cp '.;../lib/routines.jar;../lib/log4j-slf4j-impl-2.17.1.jar;../lib/log4j-api-2.17.1.jar;../lib/log4j-core-2.17.1.jar;../lib/ojdbc8-19.19.0.0.jar;../lib/xercesImpl-2.12.2.jar;../lib/xml-apis-1.4.01.jar;../lib/crypto-utils-7.1.16.jar;../lib/TalendSAX.jar;../lib/jboss-marshalling-2.0.12.Final.jar;../lib/slf4j-api-1.7.34.jar;../lib/talendcsv-1.1.0.jar;../lib/talend_file_enhanced-1.3.jar;../lib/dom4j-2.1.3.jar;../lib/talend-oracle-timestamptz.jar;recconsolidado_4_0.jar;mail_1_3.jar;' rep_diario_recaudo.recconsolidado_4_0.RecConsolidado --context=Default $args
