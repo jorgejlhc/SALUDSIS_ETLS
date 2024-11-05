8dca21cd73142f9a7f034608c06dcaf420fef370
jorgejlhc <jorgejlhc@gmail.com>
2024-11-05 01:50:31 -0500

    Versionamiento ETL APS_CARGUE_PRODUCTIVIDAD

---
+++ APS_CARGUE_PRODUCTIVIDAD/APS_CARGUE_PRODUCTIVIDAD/poms/jobs/process/APS/cargueproductividad_0.1/src/main/templates/Job_run_template.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' -cp '.;../lib/routines.jar;../lib/log4j-slf4j-impl-2.17.1.jar;../lib/log4j-api-2.17.1.jar;../lib/log4j-core-2.17.1.jar;../lib/ojdbc8-19.19.0.0.jar;../lib/xercesImpl-2.12.2.jar;../lib/xml-apis-1.4.01.jar;../lib/crypto-utils-7.1.16.jar;../lib/TalendSAX.jar;../lib/jboss-marshalling-2.0.12.Final.jar;../lib/slf4j-api-1.7.34.jar;../lib/talendcsv-1.1.0.jar;../lib/talend_file_enhanced-1.3.jar;../lib/dom4j-2.1.3.jar;../lib/talend-oracle-timestamptz.jar;cargueproductividad_0_1.jar;mail_0_1.jar;' aps_cargue_productividad.cargueproductividad_0_1.CargueProductividad --context=Default $args
