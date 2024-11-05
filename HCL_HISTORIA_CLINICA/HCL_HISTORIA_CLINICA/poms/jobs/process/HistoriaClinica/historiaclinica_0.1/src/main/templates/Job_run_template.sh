8e2c76ad690b1d74eb7e8a26fe684f9758292706
jorgejlhc <jorgejlhc@gmail.com>
2024-11-05 01:59:53 -0500

    Versionamiento ETL HCL_HISTORIA_CLINICA

---
+++ HCL_HISTORIA_CLINICA/HCL_HISTORIA_CLINICA/poms/jobs/process/HistoriaClinica/historiaclinica_0.1/src/main/templates/Job_run_template.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd `dirname $0`
+ROOT_PATH=`pwd`
+java -Dtalend.component.manager.m2.repository=$ROOT_PATH/../lib -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/log4j-slf4j-impl-2.17.1.jar:$ROOT_PATH/../lib/log4j-api-2.17.1.jar:$ROOT_PATH/../lib/log4j-core-2.17.1.jar:$ROOT_PATH/../lib/ojdbc8-19.19.0.0.jar:$ROOT_PATH/../lib/xercesImpl-2.12.2.jar:$ROOT_PATH/../lib/xml-apis-1.4.01.jar:$ROOT_PATH/../lib/crypto-utils-7.1.16.jar:$ROOT_PATH/../lib/talend-proxy-1.0.2.jar:$ROOT_PATH/../lib/TalendSAX.jar:$ROOT_PATH/../lib/jboss-marshalling-2.0.12.Final.jar:$ROOT_PATH/../lib/slf4j-api-1.7.34.jar:$ROOT_PATH/../lib/talendcsv-1.1.0.jar:$ROOT_PATH/../lib/talend_file_enhanced-1.3.jar:$ROOT_PATH/../lib/dom4j-2.1.3.jar:$ROOT_PATH/../lib/talend-oracle-timestamptz.jar:$ROOT_PATH/historiaclinica_0_1.jar:$ROOT_PATH/mail_1_3.jar: hcl_historia_clinica.historiaclinica_0_1.HistoriaClinica --context=Default "$@"
---
+++ HCL_HISTORIA_CLINICA/HCL_HISTORIA_CLINICA/poms/jobs/process/Servicio/servicehistoriaclinica_0.2/src/main/templates/Job_run_template.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp 'null' hcl_historia_clinica.servicehistoriaclinica_0_2.ServiceHistoriaClinica $args
