ecbe80ea49992c68895b663ba5c04844d1996790
jorgejlhc <jorgejlhc@gmail.com>
2024-11-05 02:10:19 -0500

    Versionamiento ETL ODO_ANTECEDENTES_MIGRATE

---
+++ ODO_ANTECEDENTES_MIGRATE/ODO_ANTECEDENTES_MIGRATE/poms/jobs/process/odo_antecedentes_migrate_0.1/src/main/templates/Job_run_template.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Dtalend.component.manager.m2.repository="%cd%/../lib" -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/log4j-slf4j-impl-2.17.1.jar;../lib/log4j-api-2.17.1.jar;../lib/log4j-core-2.17.1.jar;../lib/ojdbc8-19.19.0.0.jar;../lib/xercesImpl-2.12.2.jar;../lib/xml-apis-1.4.01.jar;../lib/crypto-utils-7.1.16.jar;../lib/TalendSAX.jar;../lib/jboss-marshalling-2.0.12.Final.jar;../lib/slf4j-api-1.7.34.jar;../lib/talendcsv-1.1.0.jar;../lib/talend_file_enhanced-1.3.jar;../lib/dom4j-2.1.3.jar;../lib/talend-oracle-timestamptz.jar;odo_antecedentes_migrate_0_1.jar;mail_1_3.jar; odo_antecedentes_migrate.odo_antecedentes_migrate_0_1.ODO_ANTECEDENTES_MIGRATE --context=Default %*
